e7b01f1fa516426b34d586327bede02d5be9e06e
EC2 Default User <ec2-user@ip-172-31-36-209.us-east-2.compute.internal>
2018-03-01 08:41:14 +0000

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "application#hello"
+  
+  root "application#goodbye"
+  
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
+end
